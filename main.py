d152cbe51dde48fc4d0391b488e7620b388a213b
Alison Reilly <reillyamr@gmail.com>
2025-03-29 17:58:30 -0400

    added reservations, users

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import location, room
+from app.routers import location, room, reservation, user, auth_google
 
 
 
@@ -9,3 +9,6 @@ app = FastAPI()
 
 app.include_router(location.router)
 app.include_router(room.router)
+app.include_router(reservation.router)
+app.include_router(user.router)
+app.include_router(auth_google.router)
